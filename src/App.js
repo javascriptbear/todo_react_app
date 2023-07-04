639feac071469e576450fa32e72899263cf2b85c
promptdrake <95160973+promptdrake@users.noreply.github.com>
2023-07-04 13:04:15 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -180,7 +180,7 @@ export default function App() {
 							})
 						) : (
 							<Text size={'lg'} mt={'md'} color={'dimmed'}>
-								You have no tasks
+								You have no text
 							</Text>
 						)}
 						<Button
